15f564faba4b832ebae612e2fd4fc9dbb19162c7
Daywid <88506180+daywid@users.noreply.github.com>
2023-08-28 10:08:28 -0300

    Update wholesale.java

--- Study/ex/ex1/wholesale.java
+++ Study/ex/ex1/wholesale.java
@@ -1,5 +1,19 @@
 package ex.ex1;
 
-public class wholesale {
+public class wholesale extends transaction{
+    private double discount;
+
+    public wholesale(){
+    }
     
+    public wholesale(double bruteValue, double discount){
+        super(bruteValue);
+        this.discount = discount;
+    }
+
+    @Override
+    public double calculateLiquidValue(){
+        double liquidValue = bruteValue - discount;
+        return liquidValue;
+    }
 }
